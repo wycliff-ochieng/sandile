f027544a0bbc027552658d214e8c94381af09ffd
wycliff <wyckieochieng91@gmail.com>
2025-03-10 22:52:32 +0300

    3

---
+++ sql/noTransaction.sql
@@ -0,0 +1,36 @@
+/*1581. Customer Who Visited but Did Not Make Any Transactions
+Easy
+Topics
+Companies
+SQL Schema
+Pandas Schema
+Table: Visits
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| visit_id    | int     |
+| customer_id | int     |
++-------------+---------+
+visit_id is the column with unique values for this table.
+This table contains information about the customers who visited the mall.
+ 
+
+Table: Transactions
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| transaction_id | int     |
+| visit_id       | int     |
+| amount         | int     |
++----------------+---------+
+transaction_id is column with unique values for this table.
+This table contains information about the transactions made during the visit_id.
+ 
+
+Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.*/
+
+SELECT customer_id,COUNT(visit_id) AS transaction_count 
+FROM Visits v LEFT JOIN transactions t ON v.visit_id = t.visit_id 
+WHERE transaction_id IS NULL GROUP BY 1
